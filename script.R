70c20425a0268fe20089eb62e67b4ea89ae6b3f1
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-09-05 15:35:08 +0800

    new changes

--- script.R
+++ script.R
@@ -9,6 +9,26 @@ db <-DBI::dbConnect(pgdrv,
                     host="localhost", port=5432,
                     user = 'postgres',
                     password = 'password')
-
+# See tables in Database
 DBI::dbListTables(db)
 
+# See records of a table accordingly
+dbGetQuery(db, 'SELECT * FROM customers')
+dbGetQuery(db, 'SELECT * FROM products')
+dbGetQuery(db, 'SELECT * FROM customer_orders')
+dbGetQuery(db, 'SELECT * FROM order_line')
+
+# INSERT; A customer orders 2 product
+dbSendQuery(db, 
+'INSERT INTO order_line (customer_order_id, product_id, qty) 
+VALUES (1, 1, 2),  (1, 2, 3)')
+
+# UPDATE;
+dbSendQuery(db, 
+            'UPDATE order_line 
+            SET product_id = 3 
+            WHERE product_id = 1')
+
+# DLETE; Deleting order line record
+dbSendQuery(db, 'DELETE FROM order_line WHERE customer_order_id = 1')
+  
