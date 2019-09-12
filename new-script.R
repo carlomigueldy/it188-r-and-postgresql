0b27ccd09f256541fb991d69d8e07921458880d1
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-09-12 17:18:26 +0800

    update

--- new-script.R
+++ new-script.R
@@ -1,16 +1,18 @@
 # Install the package in R
-# install.packages("RPostgreSQL")
-# library(RPostgreSQL)
+install.packages("RPostgreSQL")
+library(RPostgreSQL)
 
 ## Loading required package: DBI
 pgdrv <- dbDriver(drvName = "PostgreSQL")
 
+## Setup a Database connection
 db <-DBI::dbConnect(pgdrv,
                     dbname="it188",
                     host="localhost", port=5432,
                     user = 'postgres',
                     password = 'password')
 
+## Just run this snippet too
 options(sqldf.RPostgreSQL.user ="postgres", 
         sqldf.RPostgreSQL.password ="password",
         sqldf.RPostgreSQL.dbname ="it188",
@@ -18,18 +20,18 @@ options(sqldf.RPostgreSQL.user ="postgres",
         sqldf.RPostgreSQL.port =5432)
 
 # See records of a table accordingly
+# Variable declaration for SELECT query of each tables
 showCustomers <- dbReadTable(db, c("customers"))
 showProducts <- dbReadTable(db, c("products"))
-showProducts_2 <- dbReadTable(db, c("products_2"))
 showCustomerOrders <- dbReadTable(db, c("customer_orders"))
 showOrderLines <- dbReadTable(db, c("order_line"))
 
-# overwrite=TRUE will change both data and table structure
-# When row.name=TRUE then column named row.names will be added to the table
+# Receive user input and pass them in to a variable
 cname <- readline(prompt="Enter name: ")
 caddress <- readline(prompt="Enter address: ")
 cphone <- readline(prompt="Enter phone: ")
 
+# The INSERT query
 dbSendQuery(db,
             paste("INSERT INTO customers (name, address, phone) 
                   VALUES ('", cname, "','", caddress, "',", cphone,");"))
